19980a69cc8dff663b0a6fd9daa1d2d83492f5e0
dabalyan <casbaliyan@gmail.com>
2019-12-08 17:54:14 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SvgIconDirective} from './directives/svg-icon.directive';
 
@@ -10,7 +11,8 @@ import {SvgIconDirective} from './directives/svg-icon.directive';
     SvgIconDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
