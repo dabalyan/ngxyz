af69f9b350b42a76be75faa30bae22d48dc9fac8
dabalyan <casbaliyan@gmail.com>
2019-12-08 18:02:34 +0100

    integrate ngxyz-konami with the App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SvgIconDirective} from './directives/svg-icon.directive';
 
+import {NgxyzKonamiModule} from 'ngxyz-konami';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import {SvgIconDirective} from './directives/svg-icon.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxyzKonamiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
